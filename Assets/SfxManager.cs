a83381c5df90863faf4e08e195bdacbd30ec1573
F-augustus <f.augustus@gmail.com>
2021-07-02 12:31:52 -0300

    SfxManager
    GameObjetct SfxManager criado. Cada efeito sonoro é um AudioSource separado dentro do objeto pai. A função PlaySfx() pede um int com o número correspondente ao efeito sonoro pra tocar no momento certo.

---
+++ Assets/SfxManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SfxManager : MonoBehaviour
+{
+    public AudioSource sfxAudioSource;
+
+    public AudioSource[] soundEffects;
+
+    public static SfxManager instance;
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+    public void PlaySfx(int soundToPlay)
+    {        
+        soundEffects[soundToPlay].Stop();
+        soundEffects[soundToPlay].pitch = Random.Range(.9f, 1.1f);
+        soundEffects[soundToPlay].Play();
+    }
+}
